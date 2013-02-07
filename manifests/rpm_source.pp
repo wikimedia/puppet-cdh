9b41a3f2530f70bd2328820831fada543b5d3813
Nathan Bijnens <nathan@nathan.gs>
2013-02-07 13:56:27 +0100

    CentOS 6 support

--- manifests/rpm_source.pp
+++ manifests/rpm_source.pp
@@ -1,12 +1,12 @@
 # == Class cdh4::rpm_source
 #
-# Configures an apt source list pointing at
+# Configures an rpm source list pointing at
 # Cloudera's CDH4 rpm repository.
 #
 class cdh4::rpm_source {
 	$operatingsystem_lowercase = inline_template("<%= operatingsystem.downcase %>")
 
-	file { "/etc/yum.repos.d/cdh4.list":
+	file { "/etc/yum.repos.d/cdh4.repo":
 	    content => "[cloudera-cdh4] \nname=Cloudera's Distribution for Hadoop, Version 4 \nbaseurl=http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/4/ \ngpgkey = http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera \ngpgcheck = 1",
 		mode    => 0444,
 		ensure  => 'present',
@@ -14,7 +14,7 @@ class cdh4::rpm_source {
 
 	exec { "import_cloudera_yum_key":
 		command   => "sudo rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera",
-		subscribe => File["/etc/yum.repos.d/cdh4.list"],
+		subscribe => File["/etc/yum.repos.d/cdh4.repo"],
 		refreshonly => true,
 	}
 
@@ -22,6 +22,6 @@ class cdh4::rpm_source {
 		command => "/usr/bin/yum check-update",
 		timeout => 240,
 		refreshonly => true,
-		subscribe => [File["/etc/yum.repos.d/cdh4.list"], Exec["import_cloudera_yum_key"]],
+		subscribe => [File["/etc/yum.repos.d/cdh4.repo"], Exec["import_cloudera_yum_key"]],
 	}
 }
