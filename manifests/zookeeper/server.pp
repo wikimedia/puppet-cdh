68cb34b8bdd28f25618d55a7daaf2fa4a408d97e
Nathan Bijnens <nathan@nathan.gs>
2013-02-07 15:05:26 +0100

    Zookeeper workaround

--- manifests/zookeeper/server.pp
+++ manifests/zookeeper/server.pp
@@ -2,7 +2,7 @@
 # Ensures that myid is in place and that the zookeep-server is running
 class cdh4::zookeeper::server {
 	# Infer the current node's myid from numbers in the hostname.
-	$myid = inline_template("<%= hostname.gsub(/[^\d]/, '').to_i %>")
+	$myid = inline_template("<%= $cdh4::zookeeper::config::$zookeeper_hosts[$hostname] %>")
 
 	# init the zookeeper data dir
 	exec { "zookeeper-server-initialize":
