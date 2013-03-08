105bb0b57112e8373a4c746ce00392bfef40795d
Nathan Bijnens <nathan@nathan.gs>
2013-03-08 09:59:12 +0100

    zookeeper-cleanup: dismiss stdout

--- manifests/zookeeper/cron.pp
+++ manifests/zookeeper/cron.pp
@@ -3,7 +3,7 @@
 class cdh4::zookeeper::cron inherits cdh4::zookeeper::config {
 
     cron { 'zookeeper-log-cleanup':
-        command => "/usr/lib/zookeeper/bin/zkCleanup.sh $cdh4::zookeeper::config::data_dir $cdh4::zookeeper::config::data_dir 4",
+        command => "/usr/lib/zookeeper/bin/zkCleanup.sh $cdh4::zookeeper::config::data_dir $cdh4::zookeeper::config::data_dir 4 > /dev/null",
         user    => root,
         hour    => 2,
         minute  => fqdn_rand( 60 ),
