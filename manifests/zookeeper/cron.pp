56e3ecae617653c45ccde6b9a66c8757d9f9ff72
Nathan Bijnens <nathan@nathan.gs>
2013-03-08 09:52:39 +0100

    zookeeper-cleanup: a cron command to cleanup the zookeeper log files

---
+++ manifests/zookeeper/cron.pp
@@ -0,0 +1,12 @@
+# == Class cdh4::zookeeper::server
+# Ensures that the Zookeeper logs are cleaned up. It will keep the last 4 logs.
+class cdh4::zookeeper::cron inherits cdh4::zookeeper::config {
+
+    cron { 'zookeeper-log-cleanup':
+        command => "/usr/lib/zookeeper/bin/zkCleanup.sh $cdh4::zookeeper::config::data_dir $cdh4::zookeeper::config::data_dir 4",
+        user    => root,
+        hour    => 2,
+        minute  => fqdn_rand( 60 ),
+    }
+
+}
