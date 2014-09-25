18b9353fc5b88049d2b098458cb829719856099d
Andrew Otto <acotto@gmail.com>
2014-09-25 15:54:32 -0400

    Use nasty exec hack to avoid mount -> file dependency issues
    Change-Id: Ia2df7d31c7470698a46c1c7ff62f1a6dee7368f0

--- manifests/hadoop/mount.pp
+++ manifests/hadoop/mount.pp
@@ -29,8 +29,31 @@ class cdh::hadoop::mount(
         default => 'allow_other,usetrash,rw',
     }
 
-    file { $mount_point:
-        ensure => 'directory',
+    # Create the mount point if it doesn't exist.
+    #
+    # If Fuse ever looses its connection to the
+    # NameNode, it seems that the mount will be
+    # left in a weird state.  When the NameNode
+    # comes back online, the mount will be just fine.
+    # However, during the period that the NameNode
+    # is offline, puppet will fail if we were to
+    # try to ensure => 'directory' on $mount_point
+    # as a File resource.  If we were to require
+    # this as a File resource, puppet would never
+    # attempt to remount after a NameNode failure,
+    # because the File resource would fail until
+    # the mount is remounted.
+    #
+    #   Error: Could not set 'directory' on ensure: File exists - /mnt/hdfs at 36:/etc/puppet/modules/cdh/manifests/hadoop/mount.pp
+    #   ...
+    #   Warning: /Stage[main]/Cdh::Hadoop::Mount/Mount[hdfs-fuse]: Skipping because of failed dependencies
+    #
+    # We can't even use unless => "test -d ${mount_point}"
+    # in an exec, because that will fail too.
+    # Instead, we parse the output of ls. :(
+    exec { 'mkdir_hdfs_mount_point':
+        command => "/bin/mkdir -p ${mount_point}",
+        unless  => "/bin/ls $(/usr/bin/dirname ${mount_point}) 2> /dev/null | grep -q $(/usr/bin/basename ${mount_point})",
     }
 
     mount { 'hdfs-fuse':
@@ -42,6 +65,6 @@ class cdh::hadoop::mount(
         dump     => '0',
         pass     => '0',
         remounts => false,
-        require  => [Package['hadoop-hdfs-fuse'], File[$mount_point]],
+        require  => [Package['hadoop-hdfs-fuse'], Exec['mkdir_hdfs_mount_point']],
     }
 }
