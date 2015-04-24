2d581e99435b53718f2a8b6dd1045ba8062318bd
Andrew Otto <acotto@gmail.com>
2015-04-24 16:31:48 -0400

    Use cgcreate to create a CPU cgroup for Impalad
    Bug: T96329
    Change-Id: I6b65eb32e5ff33dccb27222ed5133f2a5fcec89e

--- manifests/impala/worker.pp
+++ manifests/impala/worker.pp
@@ -7,17 +7,23 @@ class cdh::impala::worker inherits cdh::impala {
         ensure => 'installed',
     }
 
+    # Impala uses cgroups to manage resources.
+    # Create a cgroup mount in which to Impala will manage its CPU cgroups
+    $cgroup_path = '/sys/fs/cgroup/cpu/impala'
+
+    # Installing cgroup-bin to have cgroups mounted in /sys/fs/cgroup
+    # and allow us to use cgcreate to create a CPU cgroup for Impala.
+    package { 'cgroup-bin':
+        ensure => 'installed',
+    }
+    exec { 'cgroup-create-impala':
+        command => '/usr/bin/cgcreate -a impala:impala -t impala:impala  -g cpu:impala',
+        creates => "${cgroup_path}/tasks",
+        require => [Package['impala-server'], Package['cgroup-bin']],
+    }
+
     $hadoop_config_directory = $::cdh::hadoop::config_directory
     $fair_scheduler_enabled = $::cdh::hadoop::fair_scheduler_enabled
-
-    # Create a path in which to store Cgroups for Impala.
-    $cgroup_path = '/sys/fs/cgroup/impala'
-    file { $cgroup_path:
-        ensure  => 'directory',
-        owner   => 'impala',
-        group   => 'impala',
-        require => Package['impala-server'],
-    }
 
     file { '/etc/default/impala':
         content => template('cdh/impala/default-impala.erb'),
@@ -31,7 +37,7 @@ class cdh::impala::worker inherits cdh::impala {
         hasrestart => true,
         require    => [
             Package['impala-server'],
-            File[$cgroup_path],
+            Exec['cgroup-create-impala'],
             File['/etc/default/impala'],
         ]
     }
