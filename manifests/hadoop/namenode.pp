47468d4f8ecdb2dff75dd4659460a4d2c8e709e5
Ottomata <aotto@wikimedia.org>
2018-12-10 16:16:24 -0500

    Use either /usr/lib/zookeeper or /usr/share/zookeeper
    Debian installs in /usr/share, CDH in /usr/lib.
    
    Change-Id: Ib5650f175c7a211cf61e74af20cccbfdb7991021

--- manifests/hadoop/namenode.pp
+++ manifests/hadoop/namenode.pp
@@ -92,11 +92,13 @@ class cdh::hadoop::namenode {
                 Service['hadoop-hdfs-namenode'],
                 Package['zookeeper'],
             ],
+            # NOTE.  zkCli.sh from debian uses different install path than
+            # from CDH.  Add both possibilities to PATH.
+            path    => '/usr/share/zookeeper/bin:/usr/lib/zookeeper/bin',
             # Don't attempt to run this command if the znode already exists
             # or if a Java Exception is returned by the zkCli tool containing
             # the ERROR log (for example when the Zookeeper node is down).
-            unless  => "/usr/lib/zookeeper/bin/zkCli.sh \
-                -server ${zookeeper_hosts_string} \
+            unless  => "zkCli.sh -server ${zookeeper_hosts_string} \
                 stat /hadoop-ha/${::cdh::hadoop::cluster_name} 2>&1 \
                 | /bin/egrep -q 'ctime|ERROR'",
         }
